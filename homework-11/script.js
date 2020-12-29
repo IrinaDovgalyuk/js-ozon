6b2d026e6d922bc21d1b5ef33aeae2e41bf8bb9a
Irina <i20d@ya.ru>
2020-12-29 20:39:04 +0300

    hw-11-2

--- homework-11/script.js
+++ homework-11/script.js
@@ -1,3 +1,5 @@
+// первая часть
+
 const word = prompt('Напишите любое слово');
 /*const word = document.querySelector('.inputWord');
 const buttonWord = document.querySelector('.buttonWord');
@@ -17,3 +19,21 @@ function checkLength(word) {
 }
 checkLength(word);
 //buttonWord.addEventListener('click', checkLength);
+
+
+// вторая часть
+const string1 = prompt('Напишите первое слово');
+const string2 = prompt('Напишите второе слово');
+
+function concatTwoStrings(string1, string2) {
+	let string3 = `${string1} ${string2}`;
+	if (string3.length >= 20) {
+		string3 = string3.substring(0, 20) + '...';
+		return string3;
+	} else {
+		return string3;
+	}
+
+}
+let newString = concatTwoStrings(string1, string2);
+alert(newString);
